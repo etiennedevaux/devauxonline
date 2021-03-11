4b3d10aeade1a6481e6f642a7582e564a9d53c03
Stephen Clarke <60775095+etiennedevaux@users.noreply.github.com>
2021-03-11 16:55:54 +0000

    Content Reviewed
    Content reviewed and updated

--- assets/js/devauxonline.js
+++ assets/js/devauxonline.js
@@ -1,29 +1,8 @@
-var jsfileversion="0001";
-function AdaptiveScaling(){
-var x=document.getElementById("ytplayer");
-var y=document.getElementById("DefaultHomePage00");
-var z=document.getElementById("TitleHomePage00");
-var w=document.getElementById("StraplineHomePage00");
-var scal = window.devicePixelRatio;
-var bH=window.outerHeight;
-var vscal=0.5;
-x.style.height=bH*vscal;
-x.style.width=bH*vscal*16/9;
-y.style.width=bH*vscal*16/9;
-z.style.width=bH*vscal*16/9;
-w.style.width=bH*vscal*16/9;
+// Global Variables
+var jsfileversion="0003";
 
-var fsize=parseInt(bH/60);
-var elements = document.getElementsByClassName("Summary");
-
-    for (var i = 0; i < elements.length; i++){
-        elements[i].style.fontSize = fsize + "pt";
-    }
-}
-
-
-function showSection(sect) {
-var elements = document.getElementsByClassName("websection");
+function showMenuComponent(sect) {
+var elements = document.getElementsByClassName("MenuComponent");
 
     for (var i = 0; i < elements.length; i++){
         elements[i].style.display = "none";
@@ -51,3 +30,4 @@ var elements = document.getElementsByClassName("Summary");
     }
 document.getElementById("SerNo").textContent="."+jsfileversion;
 }
+
